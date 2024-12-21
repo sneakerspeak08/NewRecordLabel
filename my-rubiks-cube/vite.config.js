dcf7f0afb66a7a162781101be3ef86e436bf5e4b
sneakerspeak08 <ethanschwalbach@gmail.com>
2024-12-21 18:08:20 -0500

    Update vite.config.js

--- my-rubiks-cube/vite.config.js
+++ my-rubiks-cube/vite.config.js
@@ -1,6 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [React()],
-  build: {
-    outDir: 'dist', // ensure the output is being directed to the dist folder
-  },
+  plugins: [react()],
 });
